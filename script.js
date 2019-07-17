2599be6d90719a1139229ed04fdf0130ec5c99d1
Gabriela Pinheiro <gabipinheiro38@gmail.com>
2019-07-17 01:04:26 -0300

    criar while contendo possível resolução

--- script.js
+++ script.js
@@ -7,34 +7,32 @@ a cada erro, o usuário perde 1 vida, ele tem 10 vidas*/
 
 let numeroSorteado = 0;
 let tentativas = 10;
-let i = 0;
 
 
-function rodarJogo(){
-    let valorUsuario = Number(prompt('Adivinhe o número sorteado entre 0 e 50.'));
+function rodarJogo() {
+    while (tentativas>0) {
+        let palpite = Number(prompt('Adivinhe o número sorteado entre 0 e 50.'));
 
-    if(valorUsuario === numeroSorteado){
-        alert('parabens, você ganhou o jogo');
-    }
-    else{
-        if(valorUsuario > numeroSorteado){
-            alert('seu palpite foi maior que o número, tente novamente');
-           
+        if (palpite === numeroSorteado) {
+            alert('parabens, você ganhou o jogo');
+            tentativas = 0; //não perde nenhuma vida
         }
-        else{
-            alert('seu palpite foi menor que o número, tente um número mais alto.');
-            
+        else {
+            if (palpite > numeroSorteado) {
+                alert('seu palpite foi maior que o número, tente novamente');
+            }
+            else {
+                alert('seu palpite foi menor que o número, tente um número mais alto.');
+            }
+            tentativas--; //diminui as chances a cada palpite errado
+            rodarJogo();
         }
-        rodarJogo();
     }
-    while(tentativas>0){
-        
 }
 
-
-function iniciarJogo(){
-    numeroSorteado = aleatorio (0, 50);
+function iniciarJogo() {
+    numeroSorteado = aleatorio(0, 50);
     rodarJogo();
 }
 
-rodarJogo();
+iniciarJogo();
