9306f90c1488564a21b1d1bea7274f1a0c96122d
Gabriela Pinheiro <gabipinheiro38@gmail.com>
2019-07-16 21:44:46 -0300

    adicionando primeira função

---
+++ aleatorio.js
@@ -0,0 +1,3 @@
+function aleatorio(minimo, maximo){
+    return Math.floor(Math.random()*(maximo - minimo + 1)) + minimo;
+}
---
+++ script.js
@@ -0,0 +1,36 @@
+/*O sistema sorteia um sistema aleatórrio
+o sistema pede um palpite para o usuário
+se acertar, venceu
+se não
+    o sistema avisa se foi maior ou menor
+a cada erro, o usuário perde 1 vida, ele tem 10 vidas*/
+
+let numeroSorteado = 0;
+
+
+function rodarJogo(){
+    let valorUsuario = Number(prompt('Adivinhe o número sorteado entre 0 e 50.'));
+
+    if(valorUsuario === numeroSorteado){
+        alert('parabens, você ganhou o jogo');
+    }
+    else{
+        if(valorUsuario > numeroSorteado){
+            alert('seu palpite foi maior que o número, tente novamente');
+           
+        }
+        else{
+            alert('seu palpite foi menor que o número, tente um número mais alto.');
+            
+        }
+        rodarJogo();
+    }
+}
+
+
+function iniciarJogo(){
+    numeroSorteado = aleatorio (0, 50);
+    rodarJogo();
+}
+
+rodarJogo();
